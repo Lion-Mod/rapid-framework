d02ef2d07a49025374e4a16ace0c51b4533db25f
Lion-Mod <70057706+Lion-Mod@users.noreply.github.com>
2020-09-30 08:43:07 +0100

    Added descriptions, more comments and get_encoders

--- CategoricalPreprocess.py
+++ CategoricalPreprocess.py
@@ -1,8 +1,9 @@
 from cuml.preprocessing import LabelEncoder, OneHotEncoder
 import cudf
 from cudf.core.reshape import get_dummies
+from pandas import read_csv
 
-class CategoricalPreprocess:
+class CategoricalFeatures:
     def __init__(self, df, lbl_enc_feats, ohe_feats, target_enc_feats, handle_na = False):
       """
       df: pandas dataframe
@@ -15,24 +16,68 @@ class CategoricalPreprocess:
       self.ohe_feats = ohe_feats
       self.target_enc_feats = target_enc_feats
       self.handle_na = handle_na
-      self.columns = self.df.columns
+      self.encoders = {"one hot encoded": self.ohe_feats,
+                       "label encoded": self.lbl_enc_feats,
+                       "target encoded": self.target_enc_feats}
 
       if self.handle_na:
-          for feat in self.df.columns:
-              self.df.loc[:, feat] = self.df.loc[:, feat].astype(str).fillna("-9999999")
+          if self.lbl_enc_feats != None:
+            for feat in self.lbl_enc_feats:
+              self.df[feat] = self.df[feat].astype(str).fillna("MISSING")
+
+          if self.ohe_feats != None:
+            for feat in self.ohe_feats:
+              self.df[feat] = self.df[feat].astype(str).fillna("MISSING")
+
+          if self.target_enc_feats != None:
+            for feat in self.target_enc_feats:
+              self.df[feat] = self.df[feat].astype(str).fillna("MISSING")
+    
         
       self.output_df = self.df.copy()
 
-    def one_hot_encoder(self):
-        self.output_df = cudf.get_dummies(self.output_df, columns = self.ohe_feats)
-# FIX is to update the df in the method
+    def one_hot_encoder(self, dummy_nas = None):
+      """
+      Takes the output_df and creates dummifies any features in ohe_feats list
+
+      By default it won't dummy any NAs in the features but this can be tweaked to True to handle them
+      Params
+      - dummy_nas = True/False (default to False), used to indicate if get_dummies will dummy NAs
+      """
+
+      # Check if dummy_nas if the default (None), if it is then set dummy_nas to False (i.e. don't dummy NAs) 
+      if dummy_nas == None:
+        dummy_nas = False
+
+      # Otherwise set to True (dummy NAs)  
+      else:
+        dummy_nas = True
+
+      self.output_df = cudf.get_dummies(self.output_df, 
+                                          columns = self.ohe_feats, 
+                                          dummy_na = dummy_nas)
 
     def label_encoder(self):
+      """
+      Takes the output_df and label encode any featrues in lbl_enc_feats list
+      """  
       for feat in self.lbl_enc_feats:
         le = LabelEncoder()
-        le.fit(self.df[feat])
+        le.fit(self.output_df[feat])
         self.output_df[feat] = le.transform(self.output_df[feat])
 
+    
+
+    # Target encoder
+
     def preprocess(self):
+      """
+      Returns the preprocessed output dataframe (output_df)
+      """
       return self.output_df
-    #def target_encoder(self, target):
+
+    def get_encoders(self):
+      """
+      Returns a dictionary of the features and which encoder they fall under
+      """ 
+      return self.encoders
