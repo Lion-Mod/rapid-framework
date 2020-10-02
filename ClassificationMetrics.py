910a92f68c2366d908199b45bf6562bc474efe49
Lion-Mod <70057706+Lion-Mod@users.noreply.github.com>
2020-10-02 07:31:19 +0100

    Update ClassificationMetrics.py

--- ClassificationMetrics.py
+++ ClassificationMetrics.py
@@ -1,14 +1,23 @@
 from cuml.metrics import accuracy_score, confusion_matrix
 
 class ClassificationMetrics:
-  def __init__(self, actuals, preds):
-    self.actuals = actuals
-    self.preds = preds
+  def __init__(self):
+    self.metrics = {"accuracy" : self._accuracy,
+                    "confusion_matrix" : self._confusion_matrix}
+ 
+  def __call__(self, metric, actuals, preds):
+    if metric == "accuracy":
+      return self.metrics[metric](actuals, preds)
+    
+    elif metric == "confusion_matrix":
+      return self.metrics[metric](actuals, preds)
+  
+  @staticmethod
+  def _accuracy(actuals, preds):
+    return accuracy_score(ground_truth = actuals, predictions = preds)
 
-  def accuracy(self):
-    return accuracy_score(ground_truth = self.actuals, predictions = self.preds)
-
-  def confusion_matrix(self):
-    return confusion_matrix(y_true = self.actuals, y_pred = self.preds)
+  @staticmethod
+  def _confusion_matrix(actuals, preds):
+    return confusion_matrix(y_true = actuals, y_pred = preds)
 
   #ROC AUC SCORE
